f75348b1fd66e399f7542a2c993fab4c00178fd8
Rupunicorn <keantmpqg3t@gmail.com>
2026-05-23 18:38:05 +0530

    Refactor predict function and improve formatting

--- api.py
+++ api.py
@@ -4,33 +4,35 @@ import joblib
 
 app = Flask(__name__)
 
-# Load the model at startup
+#Load the model at startup
 model = joblib.load('fraud_model.pkl')
 
 @app.route('/predict', methods=['POST'])
 def predict():
     data = request.get_json()
 
-    # Validate
-if 'amount' not in data or 'merchant' not in data:
-        return jsonify({"error": "Missing fields"}), 400
-amount = float(data['amount'])
+    #Validate
+    if 'amount' not in data or 'merchant' not in data:
+      return jsonify({"error": "Missing fields"}), 400
+
+    amount = float(data['amount'])
     merchant = data['merchant']
 
-    # Calculate risk_score (same logic)
-risk = 0
-if amount > 1000:
-        risk += 30
-if merchant == "Midnight Mart":
-        risk += 20
-# Predict using saved model
-pred = model.predict([[amount, risk]])[0]
-    proba = model.predict_proba([[amount, risk]])[0][1]
+    #Calculate risk score(same logic)
+    risk = 0
+    if amount > 1000:
+      risk += 30
+    if merchant == 'Midnight Mart':
+      risk += 20
 
-    return jsonify({
+    # Predict using saved model
+    pred = model.predict([[amount,risk]])[0]
+    prob = model.predict_proba([[amount,risk]])[0][1]
+
+    return jsonify ({
         "is_fraud": bool(pred),
         "risk_score": risk,
-        "confidence": float(proba)
+        "confidence": float(prob)
     })
 
 if __name__ == '__main__':
